58f0214aa9dbf6d2f5e46959af2a4b1aa5771f81
Andrew Montenigro <andrew.montenigro@gmail.com>
2017-06-02 17:08:52 -0400

    Add bencode operations
    Added stub functions for encoding and decoding bencode strings
    and datatypes, to be expanded on later.

--- include/bencode.h
+++ include/bencode.h
@@ -1,3 +1,7 @@
 #pragma once
 #include <stdlib.h>
 #include <stdio.h>
+
+void* bencode_decode(const char*);
+const char* bencode_encode(void*);
+
--- source/bencode.c
+++ source/bencode.c
@@ -6,3 +6,14 @@ int main(int argc, char** argv){
 	return 0;
 }
 #endif
+
+void* bencode_decode(const char* encoded_str){
+	printf("%s\n", encoded_str);
+	return NULL;
+}
+
+const char* bencode_encode(void* target){
+	printf("Encode function not implemented yet\n");
+	return NULL;
+}
+
