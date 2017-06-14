c128a2bde0ded86882a58cdae82baee592c191d7
Andrew Montenigro <andrew.montenigro@gmail.com>
2017-06-14 11:18:30 -0400

    Create torrent metainfo struct

--- source/tinyp2p.c
+++ source/tinyp2p.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <tinyp2p.h>
 
+struct metainfo {
+	void *info;
+	char *announce;
+	char *announce_list_opt;
+	long long creation_date_opt;
+	char *comment_opt;
+	char *created_by_opt;
+	char *encoding_opt;
+};
+
 #ifdef TINYP2P_MAIN
 int main(int argc, char** argv){
 	for (int i = 0; i < argc; i++){
@@ -9,3 +19,4 @@ int main(int argc, char** argv){
 	return 0;
 }
 #endif
+
